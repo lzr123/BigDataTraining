62e87f27391bfed368af12c62674bf2226c3404d
Lzr1204 <lizhuoran1024@outlook.com>
2019-06-26 17:47:06 +0800

    Update RedisUtils.py

--- Utils/RedisUtils.py
+++ Utils/RedisUtils.py
@@ -5,7 +5,3 @@ class RedisUtils():
     @staticmethod
     def insert_url_to_redis(pipeline, item, key):
         pipeline.redis_obj.rpush('bilibili:%s' % key, item['url'])
-
-    @staticmethod
-    def get_url_from_redis(redis_obj, key):
-        redis_obj
